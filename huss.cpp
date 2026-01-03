d28ad35a52380b974e3546ddbd858863c328fa80
Hussein <hussein01060900256@gmail.com>
2026-01-03 18:25:39 +0300

    max

--- huss.cpp
+++ huss.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <array>
+#include <string.h>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+/*(10==100? cout<<"h":cout<<5);
+array<int,4> aa[4]={1,2,3,4};
+using hussein=long long;
+cout<<fmod(11.5,2); ===> %
+cout<<trunc(4.7); ===> يقص العلامه*
+toupper
+tolower
+isspase*/
+#include <iostream>
 using namespace std;
-int main ()
+int maxOfThree(int a, int b, int c)
 {
-    cout<<"husse";
-    return 0 ;
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max; 
 }
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter three integers: ";
+    cin >> num1 >> num2 >> num3;
+    cout << "Maximum of the three numbers is: " << maxOfThree(num1, num2, num3) << endl;
+    return 0;
+}   
+
+
+// This program defines a function to find the ل of three integers
